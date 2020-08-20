81b39ff0eb8cecf1be4892a1253d5bd81748d3f3
DavidDexter <dmwangi@savannahinformatics.com>
2020-08-20 13:23:29 +0300

    feat : partitioning work
    - mustgetenvvar call to exit it the env is not set. Better to have th
    server die than run with undefined required env variable. Expect deploys
    to fail if the env is required and undefined
    
    Signed-off-by: DavidDexter <dmwangi@savannahinformatics.com>

--- api_client.go
+++ api_client.go
@@ -160,11 +160,14 @@ func GetEnvVar(envVarName string) (string, error) {
 // MustGetEnvVar returns the value of the environment variable with the indicated name or panics.
 // It is intended to be used in the INTERNALS of the server when we can guarantee (through orderly
 // coding) that the environment variable was set at server startup.
+// Since the env is required, kill the app if the env is not set. In the event a variable is not super
+// required, set a sensible default or dont call this method
 func MustGetEnvVar(envVarName string) string {
 	val, err := GetEnvVar(envVarName)
 	if err != nil {
 		msg := fmt.Sprintf("mandatory environment variable %s not found", envVarName)
 		log.Panicf(msg)
+		os.Exit(1)
 	}
 	return val
 }
