986dc05b4657409310fae896c5e190d35479d47f
Dumbledore <mathenge362@gmail.com>
2020-12-17 10:28:23 +0300

    chore: refactor script to allow deletion of a single resource by id

--- commands/delete_resource.py
+++ commands/delete_resource.py
@@ -29,11 +29,63 @@ def get_session():
     return session
 
 
-def get_resource_id():
-    """Get a FHIR resource id."""
+def get_patient_everything(patient_id):
+    """Gets all the resources in the patient compartment."""
     resource_path = (
         f"{URL}/datasets/{DATASET_ID}/fhirStores/"
-        f"{FHIR_STORE_ID}/fhir/{RESOURCE_TYPE}"
+        f"{FHIR_STORE_ID}/fhir/Patient/{patient_id}"
+    )
+    resource_path += "/$everything"
+
+    session = get_session()
+
+    headers = {"Content-Type": "application/fhir+json;charset=utf-8"}
+    response = session.get(resource_path, headers=headers)
+    response.raise_for_status()
+    resource = response.json()
+
+    return resource, session
+
+
+def delete_resource(patient_id):
+    """
+    Delete a FHIR resource.
+
+    Regardless of whether the operation succeeds or
+    fails, the server returns a 200 OK HTTP status code. To check that the
+    resource was successfully deleted, search for or get the resource and
+    see if it exists.
+    """
+    resource, session = get_patient_everything(patient_id)
+    entires = resource["entry"]
+    for entry in entires:
+        resource_id = entry["resource"]["id"]
+        resource_type = entry["resource"]["resourceType"]
+        delete_resource_helper(resource_id, resource_type, session)
+    return
+
+
+def delete_resource_helper(resource_id, resource_type, session):
+    """Delete."""
+    resource_path = (
+        f"{URL}/datasets/{DATASET_ID}/fhirStores/"
+        f"{FHIR_STORE_ID}/fhir/{resource_type}/{resource_id}"
+    )
+
+    response = session.delete(resource_path)
+    if response.status_code != 200:
+        logging.info(f"Status code raised: {response.status_code}")
+        logging.debug(f"{response.json()}")
+        return
+
+    logging.info("All patient records have been deleted.")
+
+
+def get_patient_resources():
+    """Get a FHIR resource (Patient for this case)."""
+    resource_path = (
+        f"{URL}/datasets/{DATASET_ID}/fhirStores/"
+        f"{FHIR_STORE_ID}/fhir/Patient"
     )
 
     session = get_session()
@@ -43,24 +95,14 @@ def get_resource_id():
     response.raise_for_status()
 
     if response.status_code != 200:
-        logging.info(f"Status code raised: {response.status_code}")
         return
 
     resource = response.json()
-    resource_ids = []
 
-    try:
-        entires = resource["entry"]
-        for entry in entires:
-            resource_ids.append(entry["resource"]["id"])
-
-        return (resource_ids, session)
-
-    except KeyError:
-        return (resource_ids, session)
+    return resource
 
 
-def delete_resource():
+def delete_all_resources():
     """
     Delete a FHIR resource.
 
@@ -69,70 +111,58 @@ def delete_resource():
     resource was successfully deleted, search for or get the resource and
     see if it exists.
     """
-    resource_ids, session = get_resource_id()
-    return delete_resource_helper(resource_ids, session)
+    patient_ids = []
+    resource = get_patient_resources()
+    try:
+        entires = resource["entry"]
+        for entry in entires:
+            patient_ids.append(entry["resource"]["id"])
 
+        count = len(patient_ids)
+        deleted_resource = 0
+        for patient_id in patient_ids:
+            while count != delete_resource:
+                delete_resource(patient_id)
+                deleted_resource += 1
 
-def delete_resource_helper(resource_ids, session):
-    """Delete a FHIR resource helper."""
-    count = len(resource_ids)
-    deleted_resource = 0
-    for resource_id in resource_ids:
-        resource_path = (
-            f"{URL}/datasets/{DATASET_ID}/fhirStores/"
-            f"{FHIR_STORE_ID}/fhir/{RESOURCE_TYPE}/{resource_id}"
-        )
-        while count != 0:
-            response = session.delete(resource_path)
-            if response.status_code != 200:
-                logging.info(f"Status code raised: {response.status_code}")
-                logging.debug(f"{response.json()}")
-                return
-            deleted_resource += 1
+        logging.info(f"{deleted_resource} out of {count} have been deleted.")
 
-    logging.info(f"{deleted_resource} out of {count} have been deleted.")
-
-
-def get_single_resource(resource_id):
-    """Get a FHIR resource.
-
-    This function's main purpose is to confirm whether or not
-    a resource has been deleted.
-    """
-    resource_path = "{}/datasets/{}/fhirStores/{}/fhir/{}/{}".format(
-        URL, DATASET_ID, FHIR_STORE_ID, RESOURCE_TYPE, resource_id
-    )
-
-    session = get_session()
-
-    headers = {"Content-Type": "application/fhir+json;charset=utf-8"}
-
-    response = session.get(resource_path, headers=headers)
-    response.raise_for_status()
-
-    resource = response.json()
-
-    print("Got {} resource:".format(resource["resourceType"]))
-    print(json.dumps(resource, indent=2))
-
-    return resource
+    except KeyError:
+        return
 
 
 if __name__ == "__main__":
-    inp = input(
-        f"Please confirm you want to delete {RESOURCE_TYPE} "
-        f"from {URL}/datasets/{DATASET_ID}/fhirStores/"
-        f"{FHIR_STORE_ID}/fhir/ [Y/N] \n"
+    print(
+        "\033[1;37;40mPlease verify these variables from your environment before proceeding:"
     )
-    yes_options = ["YES", "Yes", "yes", "Y", "y"]
-    no_options = ["NO", "No", "no", "N", "n"]
+    print(f"\033[1;32;40m \tDATASET_ID: \t {DATASET_ID}")
+    print(f"\033[1;32;40m \tFHIR_STORE_ID \t {FHIR_STORE_ID}\n")
+    try:
+        patient_id = input(
+            "\033[1;37;40mEnter a patient id to delete a single patient record "
+            "\033[1;31;40m(pressing enter and leaving it blank will DELETE ALL \N{bomb} \N{bomb} the patient records): \n"
+        )
+        yes_options = ["YES", "Yes", "yes", "Y", "y"]
 
-    if inp in yes_options:
-        delete_resource()
+        if patient_id == "":
+            warning_input = input(
+                "\033[1;31;40mAre you sure that you want to DELETE ALL \N{bomb} \N{bomb} the patients records? [Y/N]: \n"
+            )
+            if warning_input in yes_options:
+                delete_all_resources()
+            else:
+                logging.info("\033[1;32;40mThank you for confirming")
 
-    elif inp in no_options:
-        logging.info("No resource will be deleted")
-        pass
+        else:
+            inp = input(
+                f"\033[1;37;40mPlease confirm you want to delete the Patient data with ID {patient_id} "
+                f"from {URL}/datasets/{DATASET_ID}/fhirStores/"
+                f"{FHIR_STORE_ID}/fhir/ [Y/N] \n"
+            )
+            if inp in yes_options:
+                delete_resource(patient_id)
+            else:
+                logging.info("\033[1;32;40mThank you for confirming")
 
-    else:
-        logging.info("Kindly choose a Yes or No")
+    except KeyboardInterrupt:
+        logging.info("\033[1;32;40mExiting gracefully.")
